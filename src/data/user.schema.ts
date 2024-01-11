c220ac4d23954f4e171eb4303c32f6c13eede79e
migrenaa <mimonova13@gmail.com>
2024-01-11 12:02:17 +0200

    email not required

--- src/data/user.schema.ts
+++ src/data/user.schema.ts
@@ -4,7 +4,7 @@ import { IUser } from '../models/user.model';
 export const userSchema = new Schema<IUser>({
    
   address: { type: String, required: true },
-  email:  { type: String, required: true },
+  email:  { type: String, required: false },
   nonce: { type: String, required: true }, 
   upvotesAllowed: { type: Number, default: 10, required: true }, 
 
